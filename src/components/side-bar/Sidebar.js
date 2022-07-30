c0d8b87437442325015dd0c2bb46e47c59459ddd
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-07-31 01:20:51 +0300

    styles the side-bar component

--- src/components/side-bar/Sidebar.js
+++ src/components/side-bar/Sidebar.js
@@ -1,23 +1,66 @@
-import React from 'react'
+import React from "react";
+
 
 export default function Sidebar() {
   return (
-    <div className='sidebarone'>
-          <div className='sidebar-wrapper'>
-            <div className='sidebar-menu'>
-                <h3 className='sidebar-title'>Dashboard</h3>
-                <ul className='sidebar-list'>
-                <li><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>Home</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
-</svg>Analytics</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-</svg></li>
-                
-                </ul>
-            </div>
-          </div>
+    <div className="sidebar">
+      <div className="sidebar-wrapper">
+        <div className="sidebar-menu">
+          <h3 className="sidebar-title">Dashboard</h3>
+          <ul className="sidebar-list">
+            <li className="sidebar-list-item">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon-item"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                />
+              </svg>
+              Home
+            </li>
+            <li className="sidebar-list-item">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon-item"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
+                />
+              </svg>
+              Analytics
+            </li>
+            <li className="sidebar-list-item">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon-item"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+              </svg>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
