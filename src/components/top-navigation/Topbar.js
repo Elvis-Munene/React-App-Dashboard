578b5ce0a009ae58cc9d3320da142e5d198dd954
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-07-31 00:39:42 +0300

    created the top nav bar and side nav bar

---
+++ src/components/top-navigation/Topbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BellIcon} from '@heroicons/react/solid'
+import "./Topbar"
+
+
+function Topbar() {
+  return (
+    <div className='topbar'>
+       <h1>Smart Family</h1>
+       <span><BellIcon></BellIcon></span>
+       <p>..</p>
+        
+    </div>
+  )
+}
+
+export default Topbar;
