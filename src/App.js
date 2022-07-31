ab447f2dccbc016c5076ba338986bc6e979aeb85
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-07-31 06:17:05 +0300

    fetches data from the database

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import Topbar from "./components/top-navigation/Topbar";
 import Sidebar from "./components/side-bar/Sidebar";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [chartdata, setChartData] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:3000/data")
+      .then((response) => response.json())
+      .then((data) =>
+        setChartData({
+          labels: data.map((pdata) => pdata.year),
+          datasets: [
+            {
+              label: "EDUCATION",
+              data: data.map((datap) => datap.Education),
+              backgroundColor: [
+                "rgba(255,99,132,1)",
+                "rgba(54,162,235,1)",
+                "rgba(255,206,86,1)",
+                "rgba(75,192,192,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+              ],
+              borderColor: "white",
+              borderWidth: 2,
+            },
+          ],
+        })
+      );
+  }, []);
+
+  console.log(chartdata);
   return (
     <div className="App">
       <Topbar />
