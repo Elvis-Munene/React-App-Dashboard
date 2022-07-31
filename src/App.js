b2d0c88b55b0cdf552aab43b831f9b069a7beaff
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-07-31 10:17:17 +0300

    creates and styles the Analysis page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Education from "../src/components/homepage/Education";
 import Health from "../src/components/homepage/Health";
 import Clothing from "./components/homepage/Clothing";
 import Food from "../src/components/homepage/Food";
+import Analysis from "./components/homepage/Analyticspage/Analysis";
 
 function App() {
   const [chartdata, setChartData] = useState();
   const [healthData, setHealthData] = useState();
   const [clothingData, setclothingData] = useState();
   const [foodData, setfoodData] = useState();
+  const [analyze, setAnalyze] = useState();
 
   useEffect(() => {
     fetch("http://localhost:3000/data")
@@ -143,24 +145,127 @@ function App() {
       );
   }, []);
 
+  useEffect(() => {
+    fetch("http://localhost:3000/data")
+      .then((response) => response.json())
+      .then((data) =>
+        setAnalyze({
+          labels: data.map((pdata) => pdata.year),
+          datasets: [
+            {
+              label: "EDUCATION",
+              data: data.map((datap) => datap.Education),
+              backgroundColor: [
+                "rgba(255,99,132,1)",
+                "rgba(54,162,235,1)",
+                "rgba(255,206,86,1)",
+                "rgba(75,192,192,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+              ],
+              borderColor: "white",
+              borderWidth: 2,
+            },
+            {
+              label: "Health",
+              data: data.map((datap) => datap.Health),
+              backgroundColor: [
+                "rgba(255,99,132,1)",
+                "rgba(54,162,235,1)",
+                "rgba(255,206,86,1)",
+                "rgba(75,192,192,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+              ],
+              borderColor: "white",
+              borderWidth: 2,
+            },
+            {
+              label: "clothing",
+              data: data.map((datap) => datap.clothing),
+              backgroundColor: [
+                "rgba(255,99,132,1)",
+                "rgba(54,162,235,1)",
+                "rgba(255,206,86,1)",
+                "rgba(75,192,192,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+              ],
+              borderColor: "white",
+              borderWidth: 2,
+            },
+            {
+              label: "Food",
+              data: data.map((datap) => datap.Food),
+              backgroundColor: [
+                "rgba(255,99,132,1)",
+                "rgba(54,162,235,1)",
+                "rgba(255,206,86,1)",
+                "rgba(75,192,192,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+                "rgba(54,162,235,1)",
+              ],
+              borderColor: "white",
+              borderWidth: 2,
+            }
+          ],
+          
+        })
+      );
+  }, []);
+
   return (
     <div className="App">
       <Topbar />
       <div className="container">
         <Sidebar />
-        <div className="other">
-          <div className="Doncontainer" style={{ width: 200 }}>
-            <Education plotData={chartdata} />
+        <div className="home">
+          <div className="charts">
+            <div className="Doncontainer" style={{ width: 200 }}>
+              <Education plotData={chartdata} />
+            </div>
+            <div className="Healthcontainer" style={{ width: 200 }}>
+              <Health plotData={healthData} />
+            </div>
+            <div className="clothingcontainer" style={{ width: 200 }}>
+              <Clothing plotData={clothingData} />
+            </div>
+            <div className="foodcontainer" style={{ width: 200 }}>
+              <Food plotData={foodData} />
+            </div>
           </div>
-          <div className="Healthcontainer" style={{ width: 200 }}>
-            <Health plotData={healthData} />
+          <div className="Analysis">
+          <Analysis plotData={analyze} style={{ width:700}}/>
           </div>
-          <div className="clothingcontainer" style={{ width: 200 }}>
-            <Clothing plotData={clothingData} />
-          </div>
-          <div className="foodcontainer" style={{ width: 200 }}>
-            <Food plotData={foodData} />
-          </div>
+          
         </div>
       </div>
     </div>
---
+++ src/components/homepage/Analyticspage/Analysis.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Line} from "react-chartjs-2"
+
+function Analysis({plotData}) {
+    return (
+
+        <div className='dona'>
+            <h1>Analysis</h1>
+            <Line data={plotData}/>
+        </div>
+    )
+
+
+}
+
+export default Analysis;
